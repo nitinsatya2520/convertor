97d27025a9e1ef24b77f81b0ca63cae6886da8e8
nitinsatya2520 <nitinsatya6561@gmail.com>
2024-06-13 11:33:56 +0530

    Update AgeCalculator.js

--- src/AgeCalculator.js
+++ src/AgeCalculator.js
@@ -51,7 +51,7 @@ const AgeCalculator = () => {
         )}
       
   
-  export default AgeCalculator;
+  
             <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
     
